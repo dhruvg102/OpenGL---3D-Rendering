eaaf5bbac4f370596fef97f0fb4456666f27e936
Dhruv Gupta <dhruv.gupta1500@gmail.com>
2023-11-29 19:16:57 +0000

    Adding Comments

--- OpenGL-Cookbook/src/main.cpp
+++ OpenGL-Cookbook/src/main.cpp
@@ -1,9 +1,13 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+#include <glm/ext.hpp>
 
 #include <stdio.h>
 #include <stdlib.h>
 
+
+//GLSL Code For the Vertex Shader
 static const char* shaderCodeVertex = R"(
 #version 460 core
 layout (location=0) out vec3 color;
@@ -24,6 +28,7 @@ void main()
 }
 )";
 
+//GLSL Code for the Fragment Shader
 static const char* shaderCodeFragment = R"(
 #version 460 core
 layout (location=0) in vec3 color;
@@ -36,26 +41,32 @@ void main()
 
 int main()
 {
+	//GLFW Error Callback via a simple Lambda
 	glfwSetErrorCallback(
 		[](int error, const char* description)
 		{
 			fprintf(stderr, "Error: %s\n", description);
 		}
 	);
+
+	//GLFW Initialization
 	if (!glfwInit())
 		exit(EXIT_FAILURE);
 
+	//Telling GLFW the OpenGL Version (OpenGL 4.6)
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
+	//Window Creation With GLFW
 	GLFWwindow* window = glfwCreateWindow(1024, 768, "Simple example", nullptr, nullptr);
 	if (!window)
 	{
 		glfwTerminate();
 		exit(EXIT_FAILURE);
 	}
-
+	
+	//GLFW Key Callback via a simple Lambda
 	glfwSetKeyCallback(
 		window,
 		[](GLFWwindow* window, int key, int scancode, int action, int mods)
@@ -64,10 +75,14 @@ int main()
 				glfwSetWindowShouldClose(window, GLFW_TRUE);
 		}
 	);
+
+	//Prepareing the OpenGL context and
+	//Using Glad to import all OpengGL entry points & extensions
 	glfwMakeContextCurrent(window);
 	gladLoadGL();
 	glfwSwapInterval(1);
 	
+	//Compiling and Linking shader to a program
 	const GLuint shaderVertex = glCreateShader(GL_VERTEX_SHADER);
 	glShaderSource(shaderVertex, 1, &shaderCodeVertex, nullptr);
 	glCompileShader(shaderVertex);
@@ -83,6 +98,8 @@ int main()
 	glLinkProgram(program);
 	glUseProgram(program);
 
+	//Simple Empty VAO
+	//All the vertext Data is Inside the vertex shader
 	GLuint vao;
 	glCreateVertexArrays(1, &vao);
 	glBindVertexArray(vao);
@@ -91,15 +108,22 @@ int main()
 
 	while (!glfwWindowShouldClose(window))
 	{
+		//Simple Way to setup a resizable window by
+		//reading he the width and height from GLFW
 		int width, height;
 		glfwGetFramebufferSize(window, &width, &height);
 		glViewport(0, 0, width, height);
+
+		//Clear Screen & Render Triangle
 		glClear(GL_COLOR_BUFFER_BIT);
 		glDrawArrays(GL_TRIANGLES, 0, 3);
+
+		//Swapping the Buffer  to view the render
 		glfwSwapBuffers(window);
 		glfwPollEvents();
 	}
 
+	//Cleaning Up
 	glDeleteProgram(program);
 	glDeleteShader(shaderFragment);
 	glDeleteShader(shaderVertex);
