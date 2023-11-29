74bda0b6948a00a9e56167fbbc00f6528860ff84
Dhruv Gupta <dhruv.gupta1500@gmail.com>
2023-11-29 20:16:38 +0000

    Rotating Cube With GLM and (OpenGL) DSA

--- OpenGL-Cookbook/src/main.cpp
+++ OpenGL-Cookbook/src/main.cpp
@@ -6,25 +6,72 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include <vector>
+#include <iostream>
+
+using glm::vec3;
+using glm::mat4;
+
+//Struct to Pass in Values to shader through uniform buffer
+struct PerFrameData 
+{
+	mat4 MVP;			// model - view - projection matrix
+	int isWireframe;	// set color of wireframe rendering 
+};
 
 //GLSL Code For the Vertex Shader
 static const char* shaderCodeVertex = R"(
 #version 460 core
+layout(std140, binding = 0) uniform PerFrameData {
+	uniform mat4 MVP;
+	uniform int isWireframe;
+};
 layout (location=0) out vec3 color;
-const vec2 pos[3] = vec2[3](
-	vec2(-0.6, -0.4),
-	vec2( 0.6, -0.4),
-	vec2( 0.0,  0.6)
+
+const vec3 pos[8] = vec3[8](
+	vec3(-1.0,-1.0, 1.0),
+	vec3( 1.0,-1.0, 1.0),
+	vec3( 1.0, 1.0, 1.0),
+	vec3(-1.0, 1.0, 1.0),
+
+	vec3(-1.0,-1.0,-1.0),
+	vec3( 1.0,-1.0,-1.0),
+	vec3( 1.0, 1.0,-1.0),
+	vec3(-1.0, 1.0,-1.0)
 );
-const vec3 col[3] = vec3[3](
-	vec3( 1.0, 0.0, 0.0 ),
-	vec3( 0.0, 1.0, 0.0 ),
-	vec3( 0.0, 0.0, 1.0 )
+
+const vec3 col[8] = vec3[8](
+	vec3( 1.0, 0.0, 0.0),
+	vec3( 0.0, 1.0, 0.0),
+	vec3( 0.0, 0.0, 1.0),
+	vec3( 1.0, 1.0, 0.0),
+
+	vec3( 1.0, 1.0, 0.0),
+	vec3( 0.0, 0.0, 1.0),
+	vec3( 0.0, 1.0, 0.0),
+	vec3( 1.0, 0.0, 0.0)
 );
+
+const int indices[36] = int[36](
+	// front
+	0, 1, 2, 2, 3, 0,
+	// right
+	1, 5, 6, 6, 2, 1,
+	// back
+	7, 6, 5, 5, 4, 7,
+	// left
+	4, 0, 3, 3, 7, 4,
+	// bottom
+	4, 5, 1, 1, 0, 4,
+	// top
+	3, 2, 6, 6, 7, 3
+);
+
 void main()
 {
-	gl_Position = vec4(pos[gl_VertexID], 0.0, 1.0);
-	color = col[gl_VertexID];
+	int idx = indices[gl_VertexID];
+	gl_Position = MVP * vec4(pos[idx], 1.0);
+	color = isWireframe > 0 ? vec3(0.0) : col[idx];
 }
 )";
 
@@ -38,6 +85,30 @@ void main()
 	out_FragColor = vec4(color, 1.0);
 };
 )";
+
+void ShaderErrorCheck(GLuint shader, const char* type)
+{
+	GLint isCompiled = 0;
+	glGetShaderiv(shader, GL_COMPILE_STATUS, &isCompiled);
+	if (isCompiled == GL_FALSE)
+	{
+		GLint maxLength = 0;
+		glGetShaderiv(shader, GL_INFO_LOG_LENGTH, &maxLength);
+
+		// The maxLength includes the NULL character
+		std::vector<GLchar> errorLog(maxLength);
+		glGetShaderInfoLog(shader, maxLength, &maxLength, &errorLog[0]);
+
+		printf(type);
+		for (unsigned int i = 0; i < maxLength; i++) {
+			std::cout << errorLog[i];
+		}
+		// Provide the infolog in whatever manor you deem best.
+		// Exit with failure.
+		glDeleteShader(shader); // Don't leak the shader.
+		return;
+	}
+}
 
 int main()
 {
@@ -87,9 +158,13 @@ int main()
 	glShaderSource(shaderVertex, 1, &shaderCodeVertex, nullptr);
 	glCompileShader(shaderVertex);
 
+	//ShaderErrorCheck(shaderVertex, "Vertex");
+
 	const GLuint shaderFragment = glCreateShader(GL_FRAGMENT_SHADER);
 	glShaderSource(shaderFragment, 1, &shaderCodeFragment, nullptr);
 	glCompileShader(shaderFragment);
+
+	//ShaderErrorCheck(shaderFragment, "Fragment");
 
 	const GLuint program = glCreateProgram();
 	glAttachShader(program, shaderVertex);
@@ -104,26 +179,82 @@ int main()
 	glCreateVertexArrays(1, &vao);
 	glBindVertexArray(vao);
 
+	//Buffer object to hold the data using DSA functions
+	const GLsizeiptr kBufferSize = sizeof(PerFrameData);
+	GLuint perFrameDataBuf;
+	glCreateBuffers(1, &perFrameDataBuf);
+	
+	//GL_DYNAMIC_STORAGE_BIT tells OpenGL that the
+	//content of the data store might be updated after
+	//the creation through calls to glBufferSubData()
+	glNamedBufferStorage(perFrameDataBuf, kBufferSize, nullptr, GL_DYNAMIC_STORAGE_BIT);
+	
+	//Binds a range withing the buffer oject to an index buffer target
+	//The buffer is bout to the indexed target 0, this will be used inside our shader
+	glBindBufferRange(GL_UNIFORM_BUFFER, 0, perFrameDataBuf, 0, kBufferSize);
+
+
+	//Set Clear Color
 	glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+	
+	glEnable(GL_DEPTH_TEST);
+
+	//Render a wireframe on top of the solid image without z-fighting
+	glEnable(GL_POLYGON_OFFSET_LINE);
+	glPolygonOffset(-1.0f, -1.0f);
 
 	while (!glfwWindowShouldClose(window))
 	{
+
+
 		//Simple Way to setup a resizable window by
 		//reading he the width and height from GLFW
 		int width, height;
 		glfwGetFramebufferSize(window, &width, &height);
+		const float ratio = width / (float)height;
+
+		//Clear Screen
 		glViewport(0, 0, width, height);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-		//Clear Screen & Render Triangle
-		glClear(GL_COLOR_BUFFER_BIT);
-		glDrawArrays(GL_TRIANGLES, 0, 3);
+		//Model Matrix Calculated as a rotation around the diagonal (1,1,1)
+		const mat4 m = glm::rotate(
+			glm::translate(mat4(1.0f), vec3(0.0f, 0.0f, -3.5f)),
+			float(glfwGetTime()),
+			vec3(1.0f, 1.0f, 1.0f)
+		);
+		//Projection Matrix
+		const mat4 p = glm::perspective(
+			45.0f, ratio, 0.1f, 1000.0f
+		);
+		
 
+		PerFrameData perFrameData = {
+			p * m,
+			false
+		};
+		//Render Normally
+		{
+			glNamedBufferSubData(perFrameDataBuf, 0, kBufferSize, &perFrameData);
+
+			glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+			glDrawArrays(GL_TRIANGLES, 0, 36);
+		}
+		//Render Wireframe
+		{
+			perFrameData.isWireframe = true;
+			glNamedBufferSubData(perFrameDataBuf, 0, kBufferSize, &perFrameData);
+
+			glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+			glDrawArrays(GL_TRIANGLES, 0, 36);
+		}
 		//Swapping the Buffer  to view the render
 		glfwSwapBuffers(window);
 		glfwPollEvents();
 	}
 
 	//Cleaning Up
+	glDeleteBuffers(1, &perFrameDataBuf);
 	glDeleteProgram(program);
 	glDeleteShader(shaderFragment);
 	glDeleteShader(shaderVertex);
